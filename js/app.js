104011e45605954a500cf4285fc499bb64f3bdba
Denis <44744142+ashlanderDesign@users.noreply.github.com>
2019-08-11 10:02:08 +0500

    init

---
+++ js/app.js
@@ -0,0 +1,27 @@
+var app = new Vue({
+    el: "#root",
+    data: {
+        page: "home",
+        loading: true,
+        url: location.hash
+    },
+    mounted() {
+        switch (this.url) {
+            case "#about":
+                this.page = "about"
+                break;
+            case "#home":
+                this.page = "home"
+                break;
+            case "#download":
+                this.page = "download"
+                break;
+            case "#support":
+                this.page = "support"
+                break;
+        }
+        setTimeout(() => {
+            this.loading = false;
+        }, 3000);
+    }
+});
